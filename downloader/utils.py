759ec73f5932f3dc5bffc13c98d5e29928486076
Djordje Jovanovic <jovanovic.djo@gmail.com>
2025-04-02 21:38:43 +0200

    add initial get_episode method

--- downloader/utils.py
+++ downloader/utils.py
@@ -1,5 +1,21 @@
 import os
 import subprocess
+import pandas as pd
+
+def get_episode_to_download(path):
+    df = pd.read_csv(path)
+
+    not_downloaded = df[df['downloaded'] == False]
+
+    if not_downloaded.empty:
+        return None
+    
+    episode = not_downloaded.iloc[0].to_dict()
+
+    index = not_downloaded.index[0]
+    episode['index'] = index
+
+    return episode
 
 
 def download_episode(youtube_url, output_dir):
@@ -27,51 +43,13 @@ def download_episode(youtube_url, output_dir):
     downloaded_file = os.path.join(output_dir, downloaded_files[0])
     return downloaded_file
                 
-                
 
-def convert_to_mp3(input_file, output_dir=None):
-    
-    if not os.path.exists(input_file):
-        return None
-        
-    if input_file.lower().endswith('.mp3'):
-        return input_file
-        
-    if output_dir is None:
-        output_dir = os.path.dirname(input_file)
-        
-    os.makedirs(output_dir, exist_ok=True)
-    
-    base_name = os.path.basename(input_file)
-    file_name_without_ext = os.path.splitext(base_name)[0]
-    mp3_file = os.path.join(output_dir, f"{file_name_without_ext}.mp3")
-            
-    process = subprocess.run(
-        ['ffmpeg', '-i', input_file, '-codec:a', 'libmp3lame', '-qscale:a', '1', '-y', mp3_file],
-        capture_output=True,
-        text=True,
-        check=False
-    )
-    
-    if process.returncode != 0:
-        return None
-        
-    if os.path.exists(mp3_file):
-        
-        os.remove(input_file)
-        
-        return mp3_file
-    else:
-        return None
-        
 
 if __name__ == "__main__":
     test_link = "https://www.youtube.com/watch?v=McwPB-eQ2BY"
     output_dir = "downloaded_audio"
+    input_dataset = "test_dataset.csv"
     
     downloaded_file = download_episode(test_link, output_dir)
-    if downloaded_file:
-        audio = convert_to_mp3(downloaded_file, output_dir)
-    else:
-        audio = None
+
     
