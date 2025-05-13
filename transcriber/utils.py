c768330247e9216d0eac67794be85118e8196053
Djordje Jovanovic <jovanovic.djo@gmail.com>
2025-05-13 22:07:52 +0200

    modify whisper transcriber

--- transcriber/utils.py
+++ transcriber/utils.py
@@ -1,34 +1,13 @@
 import whisper
-import speech_recognition as sr
-import torch
 import os
 
 
-
-def transcribe_whisperr(input_path, output_path, input_model):
-
-    result = whisper.transcribe(input_path, input_model)
-    
-    file_name = input_path.rpartition('\\')[2].split(".")[0]
-    print(file_name)
-
-    file_name += ".txt"
-    output_path += file_name
-
-    text_file = open(output_path, "w")
-
-    text_file.write(result["text"])
-
-    text_file.close()
-
-
-
-def transcribe_whisper(input_path, output_dir):
+def transcribe_whisper(input_path, output_dir, model):
 
     transcribed_text = ""
     file_name = input_path.rpartition('\\')[2].split(".")[0]
 
-    model = whisper.load_model("small")
+    model = whisper.load_model(model)
     
     result = model.transcribe(input_path, language="sr")
     transcribed_text = result["text"]
@@ -37,13 +16,20 @@ def transcribe_whisper(input_path, output_dir):
         file.write(transcribed_text)
     
     print(f"Transcription saved to {output_dir}")
+    print(transcribed_text)
+
     return transcribed_text
 
-
+model = "small"
 input_path = "C:\\Users\\gatz0\\Desktop\\Projects\\dp-tv-show-transcript\\data\\samples\\audio\\"
 output_path = "C:\\Users\\gatz0\\Desktop\\Projects\\dp-tv-show-transcript\\data\\samples\\whisper_small\\"
 
 single_file = "C:\\Users\\gatz0\\Desktop\\Projects\\dp-tv-show-transcript\\data\\samples\\audio\\s1ep1-Rakija.wav"
 
-text = transcribe_whisper(single_file, output_path)
-print(text)
+
+for file in os.listdir(input_path):
+    filename = os.fsdecode(file)
+    if filename.endswith(".wav"): 
+        print(filename)
+        name = os.path.join(input_path, filename)
+        transcribe_whisper(name, output_path, model)
