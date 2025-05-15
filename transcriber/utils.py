a418300f39f787cd6d63a092fb059c15c9b7c145
Djordje Jovanovic <jovanovic.djo@gmail.com>
2025-05-15 23:12:39 +0200

    modify constraint

--- transcriber/utils.py
+++ transcriber/utils.py
@@ -19,17 +19,22 @@ def transcribe_whisper(input_path, output_dir, model):
 
     for saved_file in os.listdir(output_dir):
         saved_file_name = os.fsdecode(saved_file)
+
+        print("file_name: " + file_name)
+        print("saved_file: " + saved_file)
+        print("saved_file_name: " + saved_file_name)
+
         if file_name in saved_file_name: 
             msg = saved_file_name + " is already saved"
             print(msg)
-
             return
-        
-        model = whisper.load_model(model, device=device)
-    
-    result = model.transcribe(input_path, language="sr")
-    transcribed_text = result["text"]
+        else:
+            model = whisper.load_model(model, device=device)
+            result = model.transcribe(input_path, language="sr")
+            transcribed_text = result["text"]
+            break
 
+    
     with open(output_dir + file_name, "w", encoding="utf-8") as file:
         file.write(transcribed_text)
     
